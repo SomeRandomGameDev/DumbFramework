24eff4af3a334a6918b1fe0ab544d9fb49970a52
Some Random GameDev <somerandomgamedev@gmail.com>
2012-12-29 15:40:39 +0100

    Sprite engine stub

--- src/sprengine.cpp
+++ src/sprengine.cpp
@@ -1,2 +1,74 @@
+#include <GL/glew.h>
 #include <sprengine.hpp>
 
+namespace Sprite {
+
+// Dumb design ahead.
+
+Engine::Engine(Atlas *atlas, unsigned int capacity) :
+    _atlas(atlas),
+    _vao(0),
+    _vbo(0),
+    _vertexShader(0),
+    _fragmentShader(0),
+    _geometryShader(0),
+    _program(0),
+    _texture(0),
+    _uniformTexture(0),
+    _uniformMatrix(0),
+    _matrix(glm::mat4(1.0)),
+    _last(0.0),
+    _elapsed(0.0),
+    _capacity(capacity),
+    _count(0),
+    _used(-1),
+    _free(0) {
+
+  _instance = new Instance[capacity];
+  _cell = new Cell[capacity];
+  _table = new LookupItem[capacity];
+}
+
+Engine::~Engine() {
+  delete []_instance;
+  delete []_cell;
+  delete []_table;
+
+  // Program deletion.
+  if(_program != 0) {
+    glDeleteShader(_vertexShader);
+    glDeleteShader(_fragmentShader);
+    glDeleteShader(_geometryShader);
+    glDeleteProgram(_program);
+  }
+
+  // VBO/VAO deletion.
+  if(_vao != 0) {
+    glDeleteVertexArrays(1, &_vao);
+    glDeleteBuffers(1, &_vbo);
+  }
+}
+
+Identifier Engine::create(unsigned int definitionId, unsigned int firstAnim, bool cycle) {
+  // TODO This is a stub.
+  return -1;
+}
+
+void Engine::destroy(Identifier id) {
+  // TODO This is a stub.
+}
+
+bool Engine::set(Identifier id, unsigned int animId, bool cycle, double progress) {
+  // TODO This is a stub.
+  return false;
+}
+
+void Engine::viewport(int x, int y, unsigned int width, unsigned int height) {
+  // TODO This is a stub.
+}
+
+void Engine::render() {
+  // TODO This is a stub.
+}
+
+}
--- src/sprengine.hpp
+++ src/sprengine.hpp
@@ -1,10 +1,33 @@
 #ifndef _DUMB_FW_SPRENGINE_
 #define _DUMB_FW_SPRENGINE_
 
-#include <glm/glm.hpp>
 #include <sprite.hpp>
 
 namespace Sprite {
+
+/**
+ * Sprite instance identifier.
+ * Negative values are for invalid identifiers.
+ */
+typedef int Identifier;
+
+/**
+ * Lookup table item.
+ */
+typedef struct {
+  /**
+   * Previous item (free or used).
+   */
+  unsigned int _previous;
+  /**
+   * Next item (free or used).
+   */
+  unsigned int _next;
+  /**
+   * Targeted sprite slot.
+   */
+  Identifier _target;
+} LookupItem;
 
 /**
  * Sprite instance description structure.
@@ -26,17 +49,45 @@ typedef struct {
    * Elapsed time since animation start.
    */
   double _elapsed;
+  /**
+   * Cycle flag.
+   */
+  bool _cycle;
 
   /**
-   * Position in logic space.
+   * Still flag. Useful to quickly manage animation with only one frame.
    */
-  glm::vec2 _position;
+  bool _still;
+} Instance;
+
+/**
+ * Cell to copy into 'V-Ram'.
+ */
+typedef struct {
+  /**
+   * Position.
+   */
+  GLfloat posX;
+  GLfloat posY;
 
   /**
-   * Next sprite instance.
+   * Size.
    */
-  int _next;
-} _Instance;
+  GLfloat sizeX;
+  GLfloat sizeY;
+
+  /**
+   * Top texel coordinate.
+   */
+  GLfloat topU;
+  GLfloat topV;
+
+  /**
+   * Bottom texel coordinate.
+   */
+  GLfloat bottomU;
+  GLfloat bottomV;
+} Cell;
 
 /**
  * The Sprite Engine.
@@ -44,8 +95,67 @@ typedef struct {
  */
 class Engine {
 public:
+   /**
+    * Constructor.
+    * @param atlas Backed atlas.
+    * @param capacity Maximum number of manageable sprites.
+    */
+   Engine(Atlas *atlas, unsigned int capacity);
+
+   /**
+    * Destructor.
+    */
+   ~Engine();
+
+   /**
+    * Create a new sprite instance.
+    * @param definitionId Sprite definition identifier.
+    * @param firstAnim Animation to launch.
+    * @param cycle if <code>true</code>, cycle the first animation.
+    * @return An identifier to sprite instance or a negative value in case of
+    * invalid parameters.
+    */
+   Identifier create(unsigned int definitionId, unsigned int firstAnim = 0, bool cycle=true);
+
+   /**
+    * Destroy/Remove a sprite instance for the engine.
+    * @param id Identifier of the instance to remove.
+    */
+   void destroy(Identifier id);
+
+   /**
+    * Set the sprite current animation.
+    * @param Id Sprite identifier.
+    * @param animId Animation identifier.
+    * @param cycle Does the animation need to cycle ?
+    * @param progress Time progression in current animation.
+    * @return <code>false</code> in case of invalid parameters.
+    */
+   bool set(Identifier id, unsigned int animId, bool cycle=true, double progress=0);
+
+   /**
+    * Set engine drawing viewport.
+    * @param x Center origin coordinate on X axis in pixel.
+    * @param y Center origin coordinate on Y axis in pixel.
+    * @param width Width in pixel.
+    * @param height Height in pixel.
+    */
+   void viewport(int x, int y, unsigned int width, unsigned int height);
+
+   // TODO Zoom and zoom with sprite size preservation.
+   // TODO Viewport rotation.
+
+   /**
+    * Render the sprites.
+    */
+   void render();
 
 private:
+
+  /**
+   * Associated atlas.
+   */
+  Atlas *_atlas;
 
   /**
    * Vertex Array Object identifier.
@@ -113,10 +223,34 @@ private:
   unsigned int _capacity;
 
   /**
+   * Number of managed sprites.
+   */
+  unsigned int _count;
+
+  /**
    * Sprites instances.
    */
-  _Instance *_instances;
- 
+  Instance *_instance;
+
+  /**
+   * Sprites data.
+   */
+  Cell *_cell;
+
+  /**
+   * Lookup table.
+   */
+  LookupItem *_table;
+
+  /**
+   * Pointer to first used slot.
+   */
+  int _used;
+
+  /**
+   * Pointer to first free slot.
+   */
+  int _free;
 };
 
 }
