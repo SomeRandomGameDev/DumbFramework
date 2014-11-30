5c703c5f1b6f50cc7ad883a9c4b9974c8ffa7a42
Vincent Cruz <mooz@blockos.org>
2014-11-30 22:53:08 +0100

    Updated gui test.

--- src/demo/gui.cpp
+++ src/demo/gui.cpp
@@ -5,30 +5,53 @@
 
 using namespace Framework;
 
+// Dummy "application" class
 class Dummy
 {
     public:
         Dummy() 
-            : bgcolor(0.8f, 0.6f, 0.6f, 1.0f)
+            : bgcolor(0.8f, 0.6f, 0.6f)
+            , alpha(0.75f)
             , pushed(false)
             , open(true)
-        {}
+        {
+            points.resize(4);
+        }
         void render()
         {
             // First build the interface
             ImGuiIO& io = ImGui::GetIO();
             
-            ImGui::Begin("DumbFramework + ImGui Test", &open, ImVec2(520,340), 0.75f, 0);
+            ImGui::Begin("DumbFramework + ImGui Test", &open, ImVec2(640, 340), alpha, 0);
                 if(ImGui::Button("Push me!"))
                 {
                     pushed = !pushed;
                 }
                 ImGui::SameLine();
                 ImGui::Text(pushed ? "How dare you!" : "Move along!");
+                ImGui::SliderFloat("Window alpha", &alpha, 0.0f,1.0f);
+                ImGui::ColorEdit3("Background color", glm::value_ptr(bgcolor));
+                int size = points.size();
+                ImGui::InputInt("Point count", &size, 1, 10);
+                points.resize(size);
+                ImGui::PlotHistogram("Points", &points[0], points.size(), 0, nullptr, 0.0f, 1.0f, ImVec2(0,128));
+                ImVec2 bmin = ImGui::GetItemBoxMin();
+                ImVec2 bmax = ImGui::GetItemBoxMax();
+                ImVec2 mpos = ImGui::GetMousePos();
+                
+                if( ((mpos.x > bmin.x) && (mpos.y > bmin.y)) &&
+                    ((mpos.x < bmax.x) && (mpos.y < bmax.y)) )
+                {
+                    float scaled = (mpos.x - bmin.x) / (float)(bmax.x - bmin.x);
+                    int bucket = scaled * points.size();
+                    
+                    float height = (mpos.y - bmin.y) / (float)(bmax.y - bmin.y);
+                    points[bucket] = 1.0f - height;
+                }
             ImGui::End();
             
             glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-            glClearColor(bgcolor.r, bgcolor.g, bgcolor.b, bgcolor.a);
+            glClearColor(bgcolor.r, bgcolor.g, bgcolor.b, 1.0);
             glClear(GL_COLOR_BUFFER_BIT);
             
             // Draw a cube 
@@ -38,7 +61,9 @@ class Dummy
         }
         
     public:
-        glm::vec4 bgcolor;
+        glm::vec3 bgcolor;
+        std::vector<float> points;
+        float alpha;
         bool pushed;
         bool open;
 };
@@ -59,7 +84,7 @@ int main()
     wrapper.mouse.onMouseButton +=
         [](Framework::Input::Mouse::Button button, bool state, int modifier)
         {
-            //Log_Info(Module::Base, "button %x %s %d", button, state ? "pressed " : "released", modifier);
+            Log_Info(Module::Base, "button %x %s %d", button, state ? "pressed " : "released", modifier);
         };
 
     wrapper.start();
