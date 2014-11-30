bb256d7362e74cd8313d079ffe6b7ca98bc6b208
Vincent Cruz <mooz@blockos.org>
2014-11-30 16:56:46 +0100

    ImGui demo rework.

--- src/demo/gui.cpp
+++ src/demo/gui.cpp
@@ -10,25 +10,34 @@ class Dummy
     public:
         Dummy() 
             : bgcolor(0.8f, 0.6f, 0.6f, 1.0f)
+            , pushed(false)
         {}
         void render()
         {
+            // First build the interface
             ImGuiIO& io = ImGui::GetIO();
             
-            bool dummy = true;
-            ImGui::ShowTestWindow(&dummy);
-    
+            ImGui::Text("DumbFramework ImGui integration test.");
+            if(ImGui::Button("Push me!"))
+            {
+                pushed = true;
+            }
+            ImGui::SameLine();
+            ImGui::Text(pushed ? "How dare you!" : "Move along!");
+            
             glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
             glClearColor(bgcolor.r, bgcolor.g, bgcolor.b, bgcolor.a);
             glClear(GL_COLOR_BUFFER_BIT);
             
             // Draw a cube 
             
+            // Render interface.
             ImGui::Render();
         }
         
     public:
         glm::vec4 bgcolor;
+        bool pushed;
 };
 
 int main()
@@ -41,13 +50,13 @@ int main()
     Dummy foobar;
     foobar.bgcolor.r = 0.5;
     
-    ImGuiDelegate delegate(1024, 768, "ImGui test", std::bind(std::mem_fn(&Dummy::render), foobar));
+    ImGuiDelegate delegate(1024, 768, "ImGui test", std::bind(&Dummy::render, foobar));
     Wrapper<ImGuiDelegate> wrapper(&delegate);
 
     wrapper.mouse.onMouseButton +=
         [](Framework::Input::Mouse::Button button, bool state, int modifier)
         {
-            Log_Info(Module::Base, "button %x %s %d", button, state ? "pressed " : "released", modifier);
+            //Log_Info(Module::Base, "button %x %s %d", button, state ? "pressed " : "released", modifier);
         };
 
     wrapper.start();
