a7e5666ad5dc59163335ecd1392fee8a4caad107
Vincent Cruz <mooz@blockos.org>
2015-01-04 15:58:12 +0100

    Mesh interface WIP

--- include/DumbFramework/mesh.hpp
+++ include/DumbFramework/mesh.hpp
@@ -2,5 +2,55 @@
 #define _DUMB_FW_MESH_
 
 #include <DumbFramework/geometry.hpp>
+#include <DumbFramework/boundingobjects.hpp>
+
+namespace Framework {
+    
+class Mesh
+{
+    // [todo] friend class MeshReader ?
+    public:
+        enum AttributeId
+        {
+            Position = 0,
+            Normal,
+            // [todo] tangent, bitangent, bone id+weight...
+            TexCoord,
+            AttributeCount
+        };
+    
+    public:
+        Mesh();
+        ~Mesh();
+        
+        // [todo] create mesh ?
+        
+        void destroy();
+        
+        inline Geometry::Attribute const& attribute(AttributeId id) const;
+        
+        void bind(size_t startOffset) const;
+        void unbind() const;
+        
+        inline BoundingSphere const& boundingSphere() const;
+        inline BoundingBox const& boundingBox() const;
+        
+    protected:
+        void computeVertexNormals();
+        void computeVertexTangents();
+        void computeAABB();
+        void computeBoundingSphere();
+    
+    protected:
+        VertexBuffer _vertexBuffer;
+        IndexBuffer  _indexBuffer;
+        
+        Geometry::Attribute _geometryAttributes[AttributeCount];
+        
+        BoundingSphere _sphere;
+        BoundingBox    _aabb;
+};
+    
+} // Framework
 
 #endif /* _DUMB_FW_MESH_ */
